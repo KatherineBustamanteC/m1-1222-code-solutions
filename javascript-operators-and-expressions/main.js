08c133694637f8ec3fdff8c408ca9e6d75846fdf
Katherine Bustamante <katbc13@icloud.com>
2022-12-23 04:07:27 +0000

    Completed javascript-operators-and-expressions exercise.

---
+++ javascript-operators-and-expressions/main.js
@@ -0,0 +1,49 @@
+var width = 2;
+var height = 4;
+
+var area = (width * height);
+
+console.log('value of area:', area);
+console.log('typeof area:', typeof area);
+
+var bill = 5;
+var payment = 10;
+
+var change = (payment - bill);
+
+console.log('value of change:', change);
+console.log('typeof change:', typeof change);
+
+var quizzes = 2;
+var midterm = 4;
+var final = 6;
+
+var grade = (quizzes + midterm + final) / 3;
+
+console.log('value of grade:', (quizzes + midterm + final) / 3);
+console.log('typeof grade:', typeof grade);
+
+var firstName = 'Katherine';
+var lastName = 'Bustamante';
+
+var fullName = firstName + ' ' + lastName;
+
+console.log('value of fullName:', firstName + ' ' + lastName);
+console.log('typeof fullName:', typeof fullName);
+
+var pH = 2;
+var isAcidic = pH < 7;
+
+console.log('value of isAcidic:', isAcidic);
+console.log('typeof of isAcidic', typeof isAcidic);
+
+var headCount = 2;
+var isSparta = headCount === 300;
+
+console.log('value of isSparta', isSparta);
+console.log('typeof isSparta', typeof isSparta);
+
+var motto = fullName + ' ' + 'is the GOAT';
+
+console.log('value of motto:', motto);
+console.log('typeof motto:', typeof motto);
