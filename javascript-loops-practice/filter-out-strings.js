cc9acdfc9310b4d7ba58c429c82324c81cfe40c2
Katherine Bustamante <katbc13@icloud.com>
2023-01-21 23:50:15 +0000

    Delete commented out code.

--- javascript-loops-practice/filter-out-strings.js
+++ javascript-loops-practice/filter-out-strings.js
@@ -2,8 +2,14 @@
 function filterOutStrings(values) {
   var array = [];
   for (var i = 0; i < values.length; i++) {
-    if (!isNaN(values[i])) {
+    if (values[i] === null) {
       array.push(values[i]);
+    }
+    if (!isNaN(values[i])) {
+      var x = values[i] + 0;
+      if (x === values[i]) {
+        array.push(values[i]);
+      }
     }
   } return array;
 }
