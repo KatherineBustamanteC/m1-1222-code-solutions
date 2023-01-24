75be06106d5fd90b37f6ead17b5d4a1616c50793
Katherine Bustamante <katbc13@icloud.com>
2023-01-24 03:36:58 +0000

    Completed dom-querying exercise.

---
+++ dom-querying/main.js
@@ -0,0 +1,20 @@
+var $heading = document.querySelector('h1');
+
+console.log('$heading', $heading);
+console.dir($heading);
+
+var $explanation = document.querySelector('#explanation');
+
+console.log('$explanation', $explanation);
+console.dir($explanation);
+
+var $hint = document.querySelector('.hint');
+
+console.log('$hint', $hint);
+console.log('$hint');
+
+var $paragraphs = document.querySelectorAll('p');
+console.log('$paragraphs', $paragraphs);
+
+var $links = document.querySelectorAll('.example-link');
+console.log('$links', $links);
